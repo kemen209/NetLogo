0c816307242bfe01d7e0a7b2d3799cfb083c64df
Yun Lin <kemen209@gmail.com>
2024-07-04 14:11:11 +0800

    version

--- netlogo-gui/src/main/app/Splash.scala
+++ netlogo-gui/src/main/app/Splash.scala
@@ -30,7 +30,7 @@ object Splash {
   val splash = new JLabel(image) {
     val message = {
       val date = Version.buildDate
-      val version = "Version " + Version.versionDropZeroPatch.drop("ISNetLogo ".size)
+      val version = "Version " + Version.versionDropZeroPatch.drop("NetLogo ".size)
       // hopefully avoid confusion where semi-devel people report bugs in versions that aren't
       // finished yet -- don't foreground the version number to them - ST 2/27/06
       if(date.startsWith("INTERIM DEVEL BUILD"))
